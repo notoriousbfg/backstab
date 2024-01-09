7d816dc0cca29d8101437fa87da12ddb92c35c70
Tim White <tim.white@umotif.com>
2024-01-09 18:06:19 +0000

    Updates

--- background.js
+++ background.js
@@ -1,21 +1,36 @@
-function onBefore (details) {
+function onBefore(details, blockedUrls) {
     if (details.frameId > 0) {
         return
     }
 
-    browser.tabs.update({
-        url: "pages/tab.html"
-    });
+    console.log({
+        url: details.url,
+        host: hostName(details.url)
+    })
 
-    console.log('tab updated');
+    if (blockedUrls.indexOf(hostName(details.url)) > -1) {
+        browser.tabs.update({
+            url: "pages/tab.html"
+        });
+
+        console.log('tab updated');
+    }
 }
 
-function sanitiseURL(url) {
+function hostName(url) {
     if (!url.match(/^(?:\w+:)?\/\/(\S+)$/)) {
         url = 'http://' + url;
     }
-    url = url.replace(/\s/g, '');
-    return url;
+
+    url = url.replace(/\s/g, '')
+
+    console.log(`url: ${url}`)
+
+    var hostname = (new URL(url))
+        .hostname
+        .replace(/^www\./, '');
+        
+    return hostname
 }
 
 browser.storage.sync.get("blockedUrls")
@@ -27,29 +42,26 @@ browser.storage.sync.get("blockedUrls")
         var blockedUrls = new Set([]);
         if (blockedUrlString.indexOf(',') > -1) {
             blockedUrlString.split(',').forEach(function (item) {
-                var sanitised = sanitiseURL(item)
-                var url = new URL(sanitised)
-                blockedUrls.add(url.hostname)
+                blockedUrls.add(hostName(item))
             });
         } else {
-            var sanitised = sanitiseURL(blockedUrlString)
-            var url = new URL(sanitised)
-            blockedUrls.add(url.hostname)
+            blockedUrls.add(hostName(blockedUrlString))
         }
 
-        var filters = [];
-        blockedUrls.forEach(function (item, index) {
-            filters.push({ hostContains: item });
-        })
+        // var filters = [];
+        // blockedUrls.forEach(function (item, index) {
+        //     filters.push({ hostContains: item });
+        // })
 
-        const filter = {
-            url: filters,
-        };
+        // console.log(filters)
 
-        browser.webNavigation.onBeforeNavigate.addListener(
-            onBefore,
-            filter
-        );
+        // const filter = {
+        //     url: filters,
+        // };
+
+        browser.webNavigation.onBeforeNavigate.addListener(function (details) {
+            onBefore(details, [...blockedUrls])
+        });
 
     }, function (error) {
         console.log(`Error: ${error}`);
