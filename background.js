58f6b2b22e503594b513c4787137a018f1b23e0b
Tim White <tim.white@umotif.com>
2024-01-09 19:30:34 +0000

    Fix timing bug

--- background.js
+++ background.js
@@ -1,22 +1,3 @@
-function onBefore(details, blockedUrls) {
-    if (details.frameId > 0) {
-        return
-    }
-
-    console.log({
-        url: details.url,
-        host: hostName(details.url)
-    })
-
-    if (blockedUrls.indexOf(hostName(details.url)) > -1) {
-        browser.tabs.update({
-            url: "pages/tab.html"
-        });
-
-        console.log('tab updated');
-    }
-}
-
 function hostName(url) {
     if (!url.match(/^(?:\w+:)?\/\/(\S+)$/)) {
         url = 'http://' + url;
@@ -33,36 +14,76 @@ function hostName(url) {
     return hostname
 }
 
-browser.storage.sync.get("blockedUrls")
-    .then(function (result) {
-        var blockedUrlString = result.blockedUrls
+browser.webNavigation.onBeforeNavigate.addListener(function (details) {
+    if (details.frameId > 0) {
+        return
+    }
 
-        console.log(`blocked urls: ${blockedUrlString}`)
+    browser.storage.sync.get("blockedUrls")
+        .then(function (result) {
+            var blockedUrlString = result.blockedUrls
 
-        var blockedUrls = new Set([]);
-        if (blockedUrlString.indexOf(',') > -1) {
-            blockedUrlString.split(',').forEach(function (item) {
-                blockedUrls.add(hostName(item))
-            });
-        } else {
-            blockedUrls.add(hostName(blockedUrlString))
-        }
+            console.log(`blocked urls: ${blockedUrlString}`)
 
-        // var filters = [];
-        // blockedUrls.forEach(function (item, index) {
-        //     filters.push({ hostContains: item });
-        // })
+            var blockedUrls = new Set([]);
+            if (blockedUrlString.indexOf(',') > -1) {
+                blockedUrlString.split(',').forEach(function (item) {
+                    blockedUrls.add(hostName(item))
+                });
+            } else {
+                blockedUrls.add(hostName(blockedUrlString))
+            }
 
-        // console.log(filters)
+            console.log({
+                url: details.url,
+                host: hostName(details.url)
+            })
 
-        // const filter = {
-        //     url: filters,
-        // };
+            blockedUrls = Array.from(blockedUrls)
 
-        browser.webNavigation.onBeforeNavigate.addListener(function (details) {
-            onBefore(details, [...blockedUrls])
-        });
+            if (blockedUrls.indexOf(hostName(details.url)) > -1) {
+                browser.tabs.update({
+                    url: "pages/tab.html"
+                });
 
-    }, function (error) {
-        console.log(`Error: ${error}`);
-    });
+                console.log('tab updated');
+            }
+
+        }, function (error) {
+            console.log(`error: ${error}`);
+        });    
+});
+
+// browser.storage.sync.get("blockedUrls")
+//     .then(function (result) {
+//         var blockedUrlString = result.blockedUrls
+
+//         console.log(`blocked urls: ${blockedUrlString}`)
+
+//         var blockedUrls = new Set([]);
+//         if (blockedUrlString.indexOf(',') > -1) {
+//             blockedUrlString.split(',').forEach(function (item) {
+//                 blockedUrls.add(hostName(item))
+//             });
+//         } else {
+//             blockedUrls.add(hostName(blockedUrlString))
+//         }
+
+//         // var filters = [];
+//         // blockedUrls.forEach(function (item, index) {
+//         //     filters.push({ hostContains: item });
+//         // })
+
+//         // console.log(filters)
+
+//         // const filter = {
+//         //     url: filters,
+//         // };
+
+//         browser.webNavigation.onBeforeNavigate.addListener(function (details) {
+//             onBefore(details, [...blockedUrls])
+//         });
+
+//     }, function (error) {
+//         console.log(`Error: ${error}`);
+//     });
